4c2d17d21c59b9dfd2eaa5b36128ad46a98be733
ANAS KHAN <mohammadanas0544@gmail.com>
2023-10-28 21:26:36 +0530

    flushed db and turned off debug mode

--- job_board/settings.py
+++ job_board/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@r=p1$x^3o(nref01qqz^y47j18g$fds-bz#)ih806@s0)sbu='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
